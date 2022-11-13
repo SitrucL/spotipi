05a795e0725d637600d45af06f99d42a7cce39c2
Curtis Campbell <curtis.campbell1@engie.com>
2022-11-13 20:51:09 +0000

    chore: tweaks setup script

--- setup.sh
+++ setup.sh
@@ -6,26 +6,14 @@ sudo apt-get install libopenjp2-7 python3-dbus
 echo "Installing spotipy library:"
 pip install spotipy --upgrade
 
+echo "Installing python-socketio library:"
+pip install python-socketio --upgrade
+
 echo "Installing pillow library:"
 pip install pillow --upgrade
 
 echo "Installing flask library:"
 pip install flask --upgrade
-
-echo "Enter your Spotify Client ID:"
-read spotify_client_id
-
-echo "Enter your Spotify Client Secret:"
-read spotify_client_secret
-
-echo "Enter your Spotify Redirect URI:"
-read spotify_redirect_uri
-
-echo "Enter your spotify username:"
-read spotify_username
-
-echo "Enter the full path to your spotify token:"
-read spotify_token_path
 
 install_path=$(pwd)
 
@@ -55,14 +43,12 @@ echo "...done"
 
 echo "Creating spotipi service:"
 sudo cp ./config/spotipi.service /etc/systemd/system/
-sudo sed -i -e "/\[Service\]/a ExecStart=python ${install_path}/python/displayCoverArt.py ${spotify_username} ${spotify_token_path} < /dev/zero &> /dev/null &" /etc/systemd/system/spotipi.service
+sudo sed -i -e "/\[Service\]/a ExecStart=python ${install_path}/python/displayCoverArt.py < /dev/zero &> /dev/null &" /etc/systemd/system/spotipi.service
 sudo mkdir /etc/systemd/system/spotipi.service.d
 spotipi_env_path=/etc/systemd/system/spotipi.service.d/spotipi_env.conf
 sudo touch $spotipi_env_path
 sudo echo "[Service]" >> $spotipi_env_path
-sudo echo "Environment=\"SPOTIPY_CLIENT_ID=${spotify_client_id}\"" >> $spotipi_env_path
-sudo echo "Environment=\"SPOTIPY_CLIENT_SECRET=${spotify_client_secret}\"" >> $spotipi_env_path
-sudo echo "Environment=\"SPOTIPY_REDIRECT_URI=${spotify_redirect_uri}\"" >> $spotipi_env_path
+
 sudo systemctl daemon-reload
 sudo systemctl start spotipi
 sudo systemctl enable spotipi
