645f42421cc1e1e3ab98e95c54eefaf425402d76
Curtis Campbell <curtis.campbell1@engie.com>
2022-11-25 14:53:26 +0000

    fix: sorts out identation

--- python/displayCoverArt.py
+++ python/displayCoverArt.py
@@ -12,19 +12,23 @@ from rgbmatrix import RGBMatrix, RGBMatrixOptions
 # Setup sockets
 sio = socketio.Client()
 # sio.connect('https://personal-spotify-handler.fly.dev')
-sio.connect('192.168.1.31:8080')
+sio.connect("http://10.0.0.9:8080")
 
 # Configuration file
 dir = os.path.dirname(__file__)
-filename = os.path.join(dir, '../config/rgb_options.ini')
+filename = os.path.join(dir, "../config/rgb_options.ini")
 
 # Configures logger for storing song data
-logging.basicConfig(format='%(asctime)s %(message)s',
-                    datefmt='%m/%d/%Y %I:%M:%S %p', filename='spotipy.log', level=logging.INFO)
-logger = logging.getLogger('spotipy_logger')
+logging.basicConfig(
+    format="%(asctime)s %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+    filename="spotipy.log",
+    level=logging.INFO,
+)
+logger = logging.getLogger("spotipy_logger")
 
 # automatically deletes logs more than 2000 bytes
-handler = RotatingFileHandler('spotipy.log', maxBytes=2000,  backupCount=3)
+handler = RotatingFileHandler("spotipy.log", maxBytes=2000, backupCount=3)
 logger.addHandler(handler)
 
 # Configuration for the matrix
@@ -32,35 +36,35 @@ config = configparser.ConfigParser()
 config.read(filename)
 
 options = RGBMatrixOptions()
-options.rows = int(config['DEFAULT']['rows'])
-options.cols = int(config['DEFAULT']['columns'])
-options.chain_length = int(config['DEFAULT']['chain_length'])
-options.parallel = int(config['DEFAULT']['parallel'])
-options.hardware_mapping = config['DEFAULT']['hardware_mapping']
-options.gpio_slowdown = int(config['DEFAULT']['gpio_slowdown'])
-options.panel_type = config['DEFAULT']['panel_type']
-options.brightness = int(config['DEFAULT']['brightness'])
-options.limit_refresh_rate_hz = int(config['DEFAULT']['refresh_rate'])
+options.rows = int(config["DEFAULT"]["rows"])
+options.cols = int(config["DEFAULT"]["columns"])
+options.chain_length = int(config["DEFAULT"]["chain_length"])
+options.parallel = int(config["DEFAULT"]["parallel"])
+options.hardware_mapping = config["DEFAULT"]["hardware_mapping"]
+options.gpio_slowdown = int(config["DEFAULT"]["gpio_slowdown"])
+options.panel_type = config["DEFAULT"]["panel_type"]
+options.brightness = int(config["DEFAULT"]["brightness"])
+options.limit_refresh_rate_hz = int(config["DEFAULT"]["refresh_rate"])
 
-default_image = os.path.join(dir, config['DEFAULT']['default_image'])
+default_image = os.path.join(dir, config["DEFAULT"]["default_image"])
 print(default_image)
 matrix = RGBMatrix(options=options)
 
 
-@sio.on('track_data')
+@sio.on("track_data")
 def on_message(data):
 
     print(data["currentlyPlaying"]["images"][0]["url"])
-  try:
- 
+    try:
+
         imageURL = data["currentlyPlaying"]["images"][0]["url"]
         response = requests.get(imageURL)
         image = Image.open(BytesIO(response.content))
         image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
-        matrix.SetImage(image.convert('RGB'))
+        matrix.SetImage(image.convert("RGB"))
 
     except Exception as e:
         image = Image.open(default_image)
         image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
-        matrix.SetImage(image.convert('RGB'))
+        matrix.SetImage(image.convert("RGB"))
         print(e)
